80d1a493a8ec56038dea87554ce633867a76fc2b
Cesar Olayo Espiritu <omnislahs@hotmail.com>
2024-03-14 22:23:02 -0600

    playing with swt and notes project in node

--- js_files/slidingwindow_v2.js
+++ js_files/slidingwindow_v2.js
@@ -20,25 +20,27 @@ console.log('pwwkew'.split(""))
 let acc = 'pwwkew'.split("");
 
 const twoSum = (arr, target) => {
-  let ans = new Map();
-  let result = []
+  let ans = {}
   for (let i = 0; i < arr.length; i++) {
-    let result = target - arr[i];
-    console.log("sdf", result);
-    // 9-2 = 7
-    // 9-7 = 2
-    // 9-11
-    if (ans.has(result)) {
-      result[0] = ans.get(result);
-      result[1] = i
-      return result
+    let complemento = target - arr[i];
+    // comp = 9 - arr[i=0] 2
+    // comp = 7
+
+    // comp = 9 - arr[i=1] 7 
+    // comp = 2
+    if (ans[complemento] !== undefined) {
+      // checa si en el hashmap existe el valor del completo
+      // exist el  valor de  ans[7]
+
+      // en valor ans[2] si existe
+      // entonces regresa el valor  previament guardado en la ruta ans[2] = 0
+      // regresa el un arreglo [0, 1]
+      return [ans[complemento], i]
     }
-    // ans.set('7', 0)
-    // ans.set('2', 1)
-    ans.set(result, i)
+    // ans[2] = 0 ===> {2:0}
+
+    ans[arr[i]] = i;
   }
-  console.log(result);
-  return result
 }
 
 console.log("-----",twoSum([2,7,11,15], 9))
